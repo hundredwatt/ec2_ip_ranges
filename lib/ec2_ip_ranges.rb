f15f63e55fc4b4160c92d498cab3dabf5a62da49
Jason Nochlin <hundredwatt@gmail.com>
2014-02-03 10:00:45 -0500

    Fix data file path

--- lib/ec2_ip_ranges.rb
+++ lib/ec2_ip_ranges.rb
@@ -2,7 +2,7 @@ require "ec2_ip_ranges/version"
 require "ipaddr"
 
 module Ec2IpRanges
-  RAW_DATA = File.read(File.expand_path('../data/amazon_ec2_public_ip_ranges.txt', '__FILE__')).split("\n")
+  RAW_DATA = File.read(File.expand_path('../../data/amazon_ec2_public_ip_ranges.txt', __FILE__)).split("\n")
 
   def self.ec2_ip?(ip_address_string)
     test_ip = IPAddr.new(ip_address_string)
